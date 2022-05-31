136f9ee3e930f86d4e9f9f7f3734ad884d33be98
森史憲 <mori@codingdesing.jp>
2022-05-31 10:50:07 +0900

    nuxt.config.jsの調整

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,9 +4,9 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'portfolio',
+    title: '森のポートフォリオ',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'ja',
     },
     meta: [
       { charset: 'utf-8' },
